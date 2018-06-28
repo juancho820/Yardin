f829d219ad4665896775bfc7682b0f31eb8ece20
R3vival <elsufra2012@gmail.com>
2018-06-28 11:16:29 -0500

    Creacion del Game Manager/Plugins
    Se crea el GameManager y mecanica basica, ademas se agregan 3 plugins para lograr aumentar la eficiencia del proyecto

---
+++ Yardin/Assets/Yardin/Scripts/YardinGameManager.cs
@@ -0,0 +1,25 @@
+﻿using com.ootii.Messages;
+using UnityEngine;
+
+namespace Yardin
+{
+    public class YardinGameManager : MonoBehaviour
+    {
+
+        private void Start()
+        {
+            MessageDispatcher.AddListener(YardinKeys.explore, Aiuda);
+        }
+
+        void Aiuda(IMessage rMessage)
+        {
+            Debug.Log("Explorando Hando");
+        }
+
+        public void Explore()
+        {
+            MessageDispatcher.SendMessage(YardinKeys.explore);
+        }
+    }
+}
+
---
+++ Yardin/Assets/Yardin/Scripts/YardinKeys.cs
@@ -0,0 +1,15 @@
+﻿namespace Yardin
+{
+    /// <summary>
+    /// Lista de codigos designados para envia o recibir mensajes
+    /// </summary>
+    public static class YardinKeys
+    {
+        /// <summary>
+        /// Codigo para seleccionar el modo de exploracion
+        /// </summary>
+        public const string explore = "Yardin_Explore";
+
+    }
+}
+
