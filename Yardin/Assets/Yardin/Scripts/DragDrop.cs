4a4c8fceabdb0b44192190567836cd89c35b4c8f
DESKTOP-892PSIB\EQUIPO <juanlol94@hotmail.com>
2018-07-05 23:57:33 -0500

    card

---
+++ Yardin/Assets/Yardin/Scripts/DragDrop.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DragDrop : MonoBehaviour {
+
+    private Color mouseOverColor = Color.blue;
+    private Color originalColor = Color.yellow;
+    private bool dragging = false;
+    private float distance;
+
+
+    void OnMouseEnter()
+    {
+        Debug.Log("entro");
+        GetComponent<Image>().material.color = mouseOverColor;
+    }
+
+    void OnMouseExit()
+    {
+        GetComponent<Image>().material.color = originalColor;
+    }
+
+    void OnMouseDown()
+    {
+        distance = Vector3.Distance(GetComponent<RectTransform>().pivot, Camera.main.transform.position);
+        dragging = true;
+    }
+
+    void OnMouseUp()
+    {
+        dragging = false;
+    }
+
+    void Update()
+    {
+        if (dragging)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 rayPoint = ray.GetPoint(distance);
+            GetComponent<RectTransform>().pivot = rayPoint;
+        }
+    }
+}
