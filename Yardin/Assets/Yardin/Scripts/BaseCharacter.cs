1c7a822b25e4378082670dc84057b3c5c86d3768
DESKTOP-892PSIB\EQUIPO <juanlol94@hotmail.com>
2018-07-13 17:44:07 -0500

    up

---
+++ Yardin/Assets/Yardin/Scripts/BaseCharacter.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseCharacter : MonoBehaviour {
+
+    private string characterName;
+    private string characterDescription;
+    //stats
+    private int health;
+    private int explorationSkill;
+    private int forageScavange;
+    private int build;
+    private int attack;
+    private int defense;
+
+    public string CharacterName
+    {
+        get
+        {return characterName;}
+        set
+        {characterName = value;}
+    }
+    public string CharacterDescription
+    {
+        get
+        {return characterDescription;}
+        set
+        {characterDescription = value;}
+    }
+    public int Health
+    {
+        get
+        {return health;}
+        set
+        {health = value;}
+    }
+    public int ExplorationSkill
+    {
+        get
+        { return explorationSkill; }
+        set
+        { explorationSkill = value; }
+    }
+    public int ForageScavange
+    {
+        get
+        { return forageScavange; }
+        set
+        { forageScavange = value; }
+    }
+    public int Build
+    {
+        get
+        { return build; }
+        set
+        { build = value; }
+    }
+    public int Attack
+    {
+        get
+        { return attack; }
+        set
+        { attack = value; }
+    }
+    public int Defense
+    {
+        get
+        { return defense; }
+        set
+        { defense = value; }
+    }
+}
---
+++ Yardin/Assets/Yardin/Scripts/TurnBasedCombatStateMachine.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnBasedCombatStateMachine : MonoBehaviour {
+
+    public enum BattleStates
+    {
+        START,
+        PLAYERCHOICE,
+        ENEMYCHOICE,
+        LOSE,
+        WIN
+    }
+
+    private BattleStates currentState;
+
+	// Use this for initialization
+	void Start () {
+        currentState = BattleStates.START;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        switch (currentState)
+        {
+            case (BattleStates.START):
+                break;
+            case (BattleStates.PLAYERCHOICE):
+                break;
+            case (BattleStates.ENEMYCHOICE):
+                break;
+            case (BattleStates.LOSE):
+                break;
+            case (BattleStates.WIN):
+                break;
+        }
+	}
+}
