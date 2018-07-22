bbf1798d1fb516edfe223f65f88f035119e87659
DESKTOP-892PSIB\EQUIPO <juanlol94@hotmail.com>
2018-07-21 19:02:46 -0500

    regreso

--- Yardin/Assets/Yardin/Scripts/TurnBasedCombatStateMachine.cs
+++ Yardin/Assets/Yardin/Scripts/TurnBasedCombatStateMachine.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TurnBasedCombatStateMachine : MonoBehaviour {
 
@@ -52,4 +53,8 @@ public class TurnBasedCombatStateMachine : MonoBehaviour {
                 break;
         }
 	}
+    public void Escape()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
 }
